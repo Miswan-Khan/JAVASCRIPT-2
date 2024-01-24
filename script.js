455c58224daff5f53bcead5ab215a6aa54254c7c
Miswan-Khan <155727857+Miswan-Khan@users.noreply.github.com>
2024-01-24 23:53:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Assignment:2
+
+// Area of a rectangle
+var length = "20";
+var width = "10";
+
+var area = (Number(length) * Number(width));
+console.log(area);
+
+// Alert
+let username = prompt("enter your name");
+alert("WELCOME TO MY WEBPAGE, MY NAME IS M MISWAN, I AM A STUDENT, I AM LEARNING THE WEB DEVELOPMENT COURSE");
+
+console.log(username);
+// Assignment:2
